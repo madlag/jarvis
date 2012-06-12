cc51b2706e176fe88867f293ab97dc5814e8ca4f
Francois Lagunas <francois@stupeflix.com>
2012-06-12 09:22:41 +0200

    Fixed setup.py : bad README file referenced

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 import sys, os
 
 here = os.path.abspath(os.path.dirname(__file__))
-README = open(os.path.join(here, 'README.rst')).read()
+README = open(os.path.join(here, 'README.mediawiki')).read()
 NEWS = open(os.path.join(here, 'NEWS.txt')).read()
 
 
