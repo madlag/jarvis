f01aa16cfca47d3ec6f39aac3490f6cd932e2dff
Francois Lagunas <francois@stupeflix.com>
2013-10-22 15:24:24 +0200

    Bumped version number

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ README = open(os.path.join(here, 'README.rst')).read()
 NEWS = open(os.path.join(here, 'NEWS.txt')).read()
 
 
-version = '0.1.5'
+version = '0.1.6'
 
 install_requires = [
     # List your project dependencies here.
