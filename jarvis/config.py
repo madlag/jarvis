f70fb0a56ba9b203047bd2fbe11de09bb72118f0
Jeremie Taboada <taboada.jeremie@gmail.com>
2014-07-09 15:41:41 +0200

    Update behaviour to only update .jarvisconfig when custom value.

--- jarvis/config.py
+++ jarvis/config.py
@@ -2,40 +2,46 @@ from ConfigParser import ConfigParser
 from os import path
 from atexit import register
 
-DEFAULT_CONFIG = """
-[graphics]
-aspect_ratio = 16./9.
-width_ratio = 0.4
-padding_bottom = 0
-font_family = 'Monaco'
-font_size = 12
-hide_slider = False
-
-[timing]
-frame_slider_step = 30.0
-frame_interval = 33.33
-audio_sync_tolerance = 1.0/60.0
+CONFIG_TEMPLATE = """
+[jarvis]
+# Add your custom config values here...
 """
+
+default_config = {
+    'aspect_ratio': 16./9.,
+    'width_ratio': 0.4,
+    'padding_bottom': 0,
+    'font_family': 'Monaco',
+    'font_size': 12,
+    'hide_slider': False,
+    'frame_slider_step': 30.0,
+    'frame_interval': 33.33,
+    'audio_sync_tolerance': 1.0/60.0,
+}
 
 # Copy default config if no config yet
 config_path = path.expanduser('~/.jarvisconfig')
 if not path.exists(config_path):
     with open(config_path, 'wb') as f:
-        f.write(DEFAULT_CONFIG)
+        f.write(CONFIG_TEMPLATE)
 
-# Load config
+# Load defaults
+for name, value in default_config.items():
+    globals()[name.upper()] = value
+
+# Update with config
 cfg = ConfigParser()
 cfg.readfp(open(config_path))
-for section in cfg.sections():
-    for name, value in cfg.items(section):
-        globals()[name.upper()] = eval(value)
+for name, value in cfg.items('jarvis'):
+    globals()[name.upper()] = eval(value)
 
 # Save config
 def save_config():
-    for section in cfg.sections():
-        for name, value in cfg.items(section):
-            value = globals()[name.upper()]
-            cfg.set(section, name, '"%s"' % value if isinstance(value, str) else str(value))
+    for name, _ in default_config.items():
+        old = cfg.get('jarvis', name) if cfg.has_option('jarvis', name) else default_config[name]
+        new = globals()[name.upper()]
+        if old != new:
+            cfg.set('jarvis', name, '"%s"' % new if isinstance(new, str) else str(new))
     with open(config_path, 'wb') as f:
         cfg.write(f)
 register(save_config)
